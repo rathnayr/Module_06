e686705a1467c2597fd9c89126cfcbaa75380bfc
rathnayr <rathnayr@metropolia.fi>
2024-09-08 19:03:06 +0530

    Answers

---
+++ Task 04.py
@@ -0,0 +1,9 @@
+def Sum_of_number_list(numbers):
+    return sum(numbers)
+
+def main():
+    My_Numbers = (2,4,6,8,10)
+    Total_sum = Sum_of_number_list(My_Numbers)
+    print(f"Sum of the {My_Numbers} is: {Total_sum}")
+
+main()
